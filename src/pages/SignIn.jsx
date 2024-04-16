155648d5b072766b79c1a0033d059a70244ab1cd
Anil <anil6996f@gmail.com>
2024-04-17 00:48:06 +0530

    Completed sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { toast } from 'react-toastify';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn () {
   const [showPassword, setShowPassword] = useState(false);
@@ -9,7 +12,7 @@ export default function SignIn () {
     email: "",
     password: "",
   });
-
+  const navigate = useNavigate();
   const { email, password } = formData;
 
   function onChange (e) {
@@ -17,6 +20,19 @@ export default function SignIn () {
       ...prev,
       [e.target.id]: e.target.value,
     }));
+  }
+
+  async function onSubmit (e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+      if (userCredentials.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      toast.error("User Credentials do not check out!!")
+    }
   }
 
   return (
@@ -27,7 +43,7 @@ export default function SignIn () {
           <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='w-full rounded-2xl' alt="" />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onChange} placeholder='Email address' />
             <div className='relative mb-6'>
               <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type={showPassword ? "text" : "password"} id='password' value={password} onChange={onChange} placeholder='Password' />
