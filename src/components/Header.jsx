7748bb2d1bad3a0cd47e55be3d205123dcb929dc
Anil <anil6996f@gmail.com>
2024-04-25 21:11:44 +0530

    Fixed some bugs in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,70 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import React, { useEffect, useState } from 'react'
-import {  useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-
-export default function Header () {
+export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
-  const [pageState, setPageState] = useState("Sign In");
+  const [pageState, setPageState] = useState('Sign In');
   const auth = getAuth();
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        setPageState("Profile");
+        setPageState('Profile');
       } else {
-        setPageState("Sign In");
+        setPageState('Sign In');
       }
-    })
-  },[auth])
+    });
+  }, [auth]);
 
-  function pathMatchRoute (route)  {
-    if (route === location.pathname) {
-      return true
-    } 
+  function pathMatchRoute(route) {
+    if (location.pathname === route || location.pathname === `${route}/`) {
+      return true;
+    }
+    return false;
   }
-  
+
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
-        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-            <div>
-                <img src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" className='h-5 cursor-pointer' alt="logo" onClick={()=>navigate('/')}/>  
-              </div> 
-              <div>
-                <ul className='flex space-x-10'>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate('/')}>Home</li>
-            
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-            ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`} onClick={() => navigate('/offers')}>Offers</li>
-            
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-            ${(pathMatchRoute('/sign-in') || pathMatchRoute("/profile")) && 'text-black border-b-red-500'}`} onClick={() => navigate('/profile')}>{pageState}</li>
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg"
+            className="h-5 cursor-pointer"
+            alt="logo"
+            onClick={() => navigate('/')}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute('/') && 'text-black border-b-red-500'
+              }`}
+              onClick={() => navigate('/')}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute('/offers') && 'text-black border-b-red-500'
+              }`}
+              onClick={() => navigate('/offers')}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute('/profile') && 'text-black border-b-red-500'
+              }`}
+              onClick={() => navigate('/profile')}
+            >
+              {pageState}
+            </li>
           </ul>
         </div>
-        </header>  
+      </header>
     </div>
-  )
+  );
 }
